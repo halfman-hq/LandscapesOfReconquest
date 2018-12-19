c011b0007d8ae96f7b156c3b902d86a19d0bc290
Joel Gethin Lewis <me@joelgethinlewis.com>
2018-12-19 17:02:21 +0400

    Adding output variable to every method

--- docs/whereAmIHeading/script.js
+++ docs/whereAmIHeading/script.js
@@ -11,8 +11,6 @@ var options = {
   maximumAge: 0 //integer (milliseconds]) - amount of time before the error callback is invoked, if 0 it will never invoke.
 };
 
-var output = document.getElementById("out");
-
 function success(position) {
   //https://developer.mozilla.org/en-US/docs/Web/API/Coordinates
   var latitude  = position.coords.latitude;
@@ -22,6 +20,7 @@ function success(position) {
   var accuracy = position.coords.accuracy;
   var altitude = position.coords.altitude;
   var altitudeAccuracy = position.coords.altitudeAccuracy;
+  var output = document.getElementById("out");
 
   output.innerHTML = '<p>Latitude is ' + latitude + '° <br>Longitude is ' + longitude + '°' + '<br>Speed is ' + speed + '<br>Heading is ' + heading + '<br>Accuracy is ' + accuracy + '<br>Altitude is ' + altitude + '<br>Altitude accuracy is ' + altitudeAccuracy + '</p>';
 
@@ -33,6 +32,7 @@ function success(position) {
 }
 
 function error() {
+  var output = document.getElementById("out");
   output.innerHTML = "Unable to retrieve your location";
 }
 
@@ -41,6 +41,7 @@ function getLocation() {
     //navigator.geolocation.getCurrentPosition(success, error, options);
     navigator.geolocation.watchPosition(success, error, options);
   } else {
+    var output = document.getElementById("out");
     output.innerHTML = "Geolocation is not supported by this browser.";
   }
 }
